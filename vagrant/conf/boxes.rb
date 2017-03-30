d354dc64aad0207ff091488ad1c0b2bd3d92ce83
Didac Oliveira <didac@brainupdaters.net>
2017-03-30 17:30:16 +0200

    added configs and improved Vagrantfile

--- vagrant/conf/boxes.rb
+++ vagrant/conf/boxes.rb
@@ -1,11 +1,61 @@
 
-{ :hostname => 'rear-centos', :fport => 22210 , :ip => '10.100.0.10', :mac => '080027010010', :net => 'vboxnet1', :nic => 2 , :box => 'minimal/centos7', :ram => 512 , :script => 'shell/rear_centos.sh' },
-{ :hostname => 'rear-suse', :fport => 22211 , :ip => '10.100.0.11', :mac => '080027010011', :net => 'vboxnet2', :nic => 2 , :box => 'opensuse/openSUSE-42.1-x86_64', :ram => 512 , :script => 'shell/rear_suse.sh' },
-{ :hostname => 'rear-ubuntu', :fport => 22212 , :ip => '10.100.0.12', :mac => '080027010012', :net => 'vboxnet2', :nic => 2 , :box => 'minimal/xenial64', :ram => 512 , :script => 'shell/rear_ubuntu.sh' },
-{ :hostname => 'rear-debian', :fport => 22213 , :ip => '10.100.0.13', :mac => '080027010013', :net => 'vboxnet1', :nic => 2 , :box => 'minimal/jessie64', :ram => 512 , :script => 'shell/rear_debian.sh' },
-{ :hostname => 'drlm-ubuntu', :fport => 22250 , :ip => '10.100.0.50', :mac => '080027010050', :net => 'vboxnet1', :nic =>  2 , :ip2 => '10.200.0.50', :mac2 => '080027020050', :net2 => 'vboxnet2', :nic2 => 3 , :box => 'minimal/xenial64', :ram => 512 , :script => 'shell/drlm_ubuntu.sh' },
-{ :hostname => 'drlm-debian', :fport => 22260 , :ip => '10.100.0.60', :mac => '080027010060', :net => 'vboxnet1', :nic =>  2 , :ip2 => '10.200.0.60', :mac2 => '080027020060', :net2 => 'vboxnet2', :nic2 => 3 , :box => 'minimal/jessie64', :ram => 512 , :script => 'shell/drlm_debian.sh' },
+{ :hostname => 'cli-centos5', :ip => '10.100.0.10', :mac => '525400010010', :box => '', :ram => 512 , :script => 'provisioning/rear/centos5.sh' },
+{ :hostname => 'cli-centos6', :ip => '10.100.0.11', :mac => '525400010011', :box => '', :ram => 512 , :script => 'provisioning/rear/centos6.sh' },
+{ :hostname => 'cli-centos7', :ip => '10.100.0.12', :mac => '525400010012', :box => '', :ram => 512 , :script => 'provisioning/rear/centos7.sh' },
+{ :hostname => 'cli-osuse13', :ip => '10.100.0.13', :mac => '525400010013', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-osuse42', :ip => '10.100.0.14', :mac => '525400010014', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-ubuntu14', :ip => '10.100.0.15', :mac => '525400010015', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-ubuntu16', :ip => '10.100.0.16', :mac => '525400010016', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-debian6', :ip => '10.100.0.17', :mac => '525400010017', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-debian7', :ip => '10.100.0.18', :mac => '525400010018', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'cli-debian8', :ip => '10.100.0.19', :mac => '525400010019', :box => '', :ram => 512 , :script => 'provisioning/rear/.sh' },
+{ :hostname => 'srv-ubuntu', :ip => '10.100.0.20', :mac => '525400010020', :box => '', :ram => 512 , :script => 'provisioning/drlm/ubuntu.sh' },
+{ :hostname => 'srv-debian', :ip => '10.100.0.21', :mac => '525400010021', :box => '', :ram => 512 , :script => 'provisioning/drlm/debian.sh' },
 
 ## force MACs
 ## http://serverfault.com/questions/40712/what-range-of-mac-addresses-can-i-safely-use-for-my-virtual-machines
 ## https://github.com/vagrant-libvirt/vagrant-libvirt/issues/426
+
+# ReaR boxes definition
+declare -A rear
+#rear[$distro.$param]=''
+
+#debian8 client
+rear[debian8.hostname]=
+rear[debian8.ip]=
+rear[debian8.mac]=
+rear[debian8.box]=
+rear[debian8.ram]=
+rear[debian8.script]=
+rear[debian8.gport]=
+
+#ubuntu16 client
+rear[ubuntu16.hostname]=
+rear[ubuntu16.ip]=
+rear[ubuntu16.mac]=
+rear[ubuntu16.box]=
+rear[ubuntu16.ram]=
+rear[ubuntu16.script]=
+rear[ubuntu16.gport]=
+
+# DRLM boxes definition
+declare -A drlm
+#drlm[$distro.$param]=''
+
+#debian8 server
+drlm[debian8.hostname]=
+drlm[debian8.ip]=
+drlm[debian8.mac]=
+drlm[debian8.box]=
+drlm[debian8.ram]=
+drlm[debian8.script]=
+drlm[debian8.gport]=
+
+#ubuntu16 server
+drlm[ubuntu16.hostname]=
+drlm[ubuntu16.ip]=
+drlm[ubuntu16.mac]=
+drlm[ubuntu16.box]=
+drlm[ubuntu16.ram]=
+drlm[ubuntu16.script]=
+drlm[ubuntu16.gport]=
