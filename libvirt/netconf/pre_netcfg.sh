29b57522e3e3ac019ca12e5e60aaac282d40f6bd
Didac Oliveira <didac@brainupdaters.net>
2017-03-31 01:19:00 +0200

    Adjustments for datt libvirt and vagrant config

--- libvirt/netconf/pre_netcfg.sh
+++ libvirt/netconf/pre_netcfg.sh
@@ -1,18 +1,18 @@
-HOST_MAC=$(hexdump -vn3 -e '/3 "52:54:00"' -e '/1 ":%02x"' -e '"\n"' /dev/urandom)
+#HOST_MAC=$(hexdump -vn3 -e '/3 "52:54:00"' -e '/1 ":%02x"' -e '"\n"' /dev/urandom)
 
 sudo modprobe dummy
-sudo ip link add datt-dummy address "$HOST_MAC" type dummy
+#sudo ip link add datt-dummy address "$HOST_MAC" type dummy
 
-sudo brctl addbr dattnet
-sudo brctl stp dattnet on
-sudo brctl addif dattnet datt-dummy
-sudo ip address add 10.100.0.1/24 dev dattnet broadcast 10.100.0.255
+#sudo brctl addbr dattnet
+#sudo brctl stp dattnet on
+#sudo brctl addif dattnet datt-dummy
+#sudo ip address add 10.100.0.1/24 dev dattnet broadcast 10.100.0.255
 
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv4.conf.all.forwarding=1
 
-sudo iptables-save -c > /tmp/orig.iptables
-sudo iptables-restore -c < libvirt/netconf/datt.iptables
+#sudo iptables-save -c > /tmp/orig.iptables
+#sudo iptables-restore -c < libvirt/netconf/datt.iptables
 
 
 sudo virsh net-define libvirt/netconf/dattnet.xml
--- vagrant/conf/boxes_to_test.rb
+++ vagrant/conf/boxes_to_test.rb
@@ -1,3 +1,4 @@
+nodes = [
 
 { :hostname => 'cli-centos5', :ip => '10.100.0.10', :mac => '525400010010', :box => '', :ram => 512 , :script => 'provisioning/rear/centos5.sh' },
 { :hostname => 'cli-centos6', :ip => '10.100.0.11', :mac => '525400010011', :box => '', :ram => 512 , :script => 'provisioning/rear/centos6.sh' },
@@ -12,50 +13,8 @@
 { :hostname => 'srv-ubuntu', :ip => '10.100.0.20', :mac => '525400010020', :box => '', :ram => 512 , :script => 'provisioning/drlm/ubuntu.sh' },
 { :hostname => 'srv-debian', :ip => '10.100.0.21', :mac => '525400010021', :box => '', :ram => 512 , :script => 'provisioning/drlm/debian.sh' },
 
+]
+
 ## force MACs
 ## http://serverfault.com/questions/40712/what-range-of-mac-addresses-can-i-safely-use-for-my-virtual-machines
 ## https://github.com/vagrant-libvirt/vagrant-libvirt/issues/426
-
-# ReaR boxes definition
-declare -A rear
-#rear[$distro.$param]=''
-
-#debian8 client
-rear[debian8.hostname]=
-rear[debian8.ip]=
-rear[debian8.mac]=
-rear[debian8.box]=
-rear[debian8.ram]=
-rear[debian8.script]=
-rear[debian8.gport]=
-
-#ubuntu16 client
-rear[ubuntu16.hostname]=
-rear[ubuntu16.ip]=
-rear[ubuntu16.mac]=
-rear[ubuntu16.box]=
-rear[ubuntu16.ram]=
-rear[ubuntu16.script]=
-rear[ubuntu16.gport]=
-
-# DRLM boxes definition
-declare -A drlm
-#drlm[$distro.$param]=''
-
-#debian8 server
-drlm[debian8.hostname]=
-drlm[debian8.ip]=
-drlm[debian8.mac]=
-drlm[debian8.box]=
-drlm[debian8.ram]=
-drlm[debian8.script]=
-drlm[debian8.gport]=
-
-#ubuntu16 server
-drlm[ubuntu16.hostname]=
-drlm[ubuntu16.ip]=
-drlm[ubuntu16.mac]=
-drlm[ubuntu16.box]=
-drlm[ubuntu16.ram]=
-drlm[ubuntu16.script]=
-drlm[ubuntu16.gport]=
