96b7f8d17338cdfcfe2534d5d936f97fb84b214f
Didac Oliveira <didac@brainupdaters.net>
2017-03-31 15:35:22 +0200

    Added multiple commands to datt

--- libs/functions.sh
+++ libs/functions.sh
@@ -80,3 +80,17 @@ function ansi {
      echo -e "\e[${1}m${*:2}\e[0m";
 }
 
+# return whether $1 equals one of the remaining arguments
+function IsInArray() {
+    local needle="$1"
+    test -z "$needle" && return 1
+    while shift ; do
+        # at the end $1 becomes an unbound variable (after all arguments were shifted)
+        # so that an empty default value is used to avoid 'set -eu' error exit
+        # and that empty default value cannot match because needle is non-empty:
+        test "$needle" == "${1:-}" && return 0
+    done
+    return 1
+}
+
+
